cb9dd0c21975c25b15daa6a29ee0377aec151d88
Arun-Misra <kanhaam2006@gmail.com>
2026-06-18 14:51:04 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -28,7 +28,7 @@ public class Main {
         int nextJobId = 1;
         List<Job> jobs = new ArrayList<>();
         while (true) {
-
+            reapJobs(jobs);
             System.out.print("$ ");
             String cmd = s.nextLine();
             List<String> parts = parse(cmd);
@@ -175,9 +175,7 @@ public class Main {
                 }
             }
 
-            else if (parts.get(0).equals("jobs")) {
-
-    List<Job> doneJobs = new ArrayList<>();
+      else if (parts.get(0).equals("jobs")) {
 
     int last = jobs.size() - 1;
     int secondLast = jobs.size() - 2;
@@ -192,34 +190,13 @@ public class Main {
             marker = '-';
         }
 
-        if (job.process.isAlive()) {
-
-            System.out.printf(
-                    "[%d]%c  %-24s%s%n",
-                    job.id,
-                    marker,
-                    "Running",
-                    job.command);
-
-        } else {
-
-            String cmdText = job.command;
-            if (cmdText.endsWith(" &")) {
-                cmdText = cmdText.substring(0, cmdText.length() - 2);
-            }
-
-            System.out.printf(
-                    "[%d]%c  %-24s%s%n",
-                    job.id,
-                    marker,
-                    "Done",
-                    cmdText);
-
-            doneJobs.add(job);
-        }
+        System.out.printf(
+                "[%d]%c  %-24s%s%n",
+                job.id,
+                marker,
+                "Running",
+                job.command);
     }
-
-    jobs.removeAll(doneJobs);
 }
 
             else {
@@ -281,50 +258,45 @@ public class Main {
         }
     }
 
-    // static void reapJobs(List<Job> jobs) {
-    //     List<Job> doneJobs = new ArrayList<>();
+   static void reapJobs(List<Job> jobs) {
+    List<Job> doneJobs = new ArrayList<>();
 
-    //     for (Job job : jobs) {
-    //         if (!job.process.isAlive()) {
-    //             doneJobs.add(job);
-    //         }
-    //     }
+    for (Job job : jobs) {
+        if (!job.process.isAlive()) {
+            doneJobs.add(job);
+        }
+    }
 
-    //     int last = jobs.size() - 1;
-    //     int secondLast = jobs.size() - 2;
+    int last = jobs.size() - 1;
+    int secondLast = jobs.size() - 2;
 
-    //     for (int i = 0; i < jobs.size(); i++) {
-    //         Job job = jobs.get(i);
+    for (int i = 0; i < jobs.size(); i++) {
+        Job job = jobs.get(i);
 
-    //         if (!doneJobs.contains(job)) {
-    //             continue;
-    //         }
+        if (!doneJobs.contains(job))
+            continue;
 
-    //         char marker = ' ';
-    //         if (i == last) {
-    //             marker = '+';
-    //         } else if (i == secondLast) {
-    //             marker = '-';
-    //         }
+        char marker = ' ';
+        if (i == last)
+            marker = '+';
+        else if (i == secondLast)
+            marker = '-';
 
-    //         String cmdText = job.command;
-    //         if (cmdText.endsWith(" &")) {
-    //             cmdText = cmdText.substring(0, cmdText.length() - 2);
-    //         }
+        String cmdText = job.command;
+        if (cmdText.endsWith(" &")) {
+            cmdText = cmdText.substring(0, cmdText.length() - 2);
+        }
 
-    //         System.out.printf(
-    //                 "[%d]%c  %-24s%s%n",
-    //                 job.id,
-    //                 marker,
-    //                 "Done",
-    //                 cmdText);
-    //     }
+        System.out.printf(
+                "[%d]%c  %-24s%s%n",
+                job.id,
+                marker,
+                "Done",
+                cmdText);
+    }
 
-    //     if (!doneJobs.isEmpty()) {
-    //         jobs.removeAll(doneJobs);
-    //     }
-    // }
-
+    jobs.removeAll(doneJobs);
+}
     static List<String> parse(String s) {
         List<String> args = new ArrayList<>();
         StringBuilder cur = new StringBuilder();
